cb2135ce56dc19a43a93b09d22e8aa15efd064ef
saverio596 <saverio.benedetto96@gmail.com>
2026-04-27 22:48:10 +0200

    creazione file sql

---
+++ migration.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS Orizon;
+USE Orizon;
+
+-- Tabella Paesi
+CREATE TABLE paesi (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+-- Tabella Viaggi
+CREATE TABLE viaggi (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    posti_disponibili INT NOT NULL,
+    creato_il TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tabella Pivot (Relazione molti-a-molti)
+CREATE TABLE viaggio_paese (
+    viaggio_id INT,
+    paese_id INT,
+    PRIMARY KEY (viaggio_id, paese_id),
+    FOREIGN KEY (viaggio_id) REFERENCES viaggi(id) ON DELETE CASCADE,
+    FOREIGN KEY (paese_id) REFERENCES paesi(id) ON DELETE CASCADE
+);
